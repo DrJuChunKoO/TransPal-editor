09a6b7646ad2d5096a27983d5129251f442049a6
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2024-03-11 11:27:32 +0800

    added useCurrentFile hook

--- src/hooks/useCurrentFile.ts
+++ src/hooks/useCurrentFile.ts
@@ -1,6 +1,92 @@
 import { useLocalStorage } from "usehooks-ts";
 
+import { pangu } from "pangu-ts";
 export default function useCurrentFile() {
+  const [panguEnabled] = useLocalStorage("pangu-enabled", true);
+  function loadFile(file: File) {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const content = e.target?.result;
+        const fileExtension = file.name.split(".").pop();
+        if (typeof content === "string" && fileExtension === "srt") {
+          // parse srt to json
+          const srt = content.split("\n\n").map((x) => x.split("\n"));
+          let result = {
+            version: "1.0",
+            info: {
+              filename: file.name,
+            },
+            raw: {
+              diarization: null,
+              transcript: null,
+              srt: content,
+            },
+            content: [] as any[],
+          };
+          let lastEnd = 0;
+
+          // 00:00:00,000
+          function toSeconds(time: string) {
+            const timeArr = time.split(":");
+            const hours = parseInt(timeArr[0]);
+            const minutes = parseInt(timeArr[1]);
+            const seconds = parseInt(timeArr[2].split(",")[0]);
+            const milliseconds = parseInt(timeArr[2].split(",")[1]);
+            return (
+              hours * 60 * 60 + minutes * 60 + seconds + milliseconds / 1000
+            );
+          }
+          srt.forEach((x) => {
+            const randomId = Math.random().toString(36).substring(7);
+            if (x.length < 3) return;
+            const textMatch = x[2].match(/(.*): (.*)/);
+            const speaker = textMatch ? textMatch[1] : "Unknown";
+            let text = textMatch ? textMatch[2] : x[2];
+            if (panguEnabled) {
+              text = pangu.spacing(text.trim());
+            }
+
+            const [start, end] = x[1].split(" --> ").map((x) => toSeconds(x));
+
+            if (start > lastEnd + 60) {
+              result.content.push({
+                start: lastEnd,
+                end: start,
+                id: randomId,
+                type: "divider",
+                text: "",
+                speaker: "",
+              });
+            }
+
+            lastEnd = end;
+
+            result.content.push({
+              start,
+              end,
+              id: randomId,
+              type: "speech",
+              text,
+              speaker,
+            });
+          });
+          setFile(result);
+        } else if (typeof content === "string" && fileExtension === "json") {
+          let res = JSON.parse(content);
+          if (panguEnabled) {
+            res.content = res.content.map((x: any) => {
+              x.text = pangu.spacing(x.text.trim());
+              return x;
+            });
+          }
+          setFile(res);
+        }
+      };
+      reader.readAsText(file);
+    }
+  }
+
   const [file, setFile] = useLocalStorage<{
     raw?: any;
     info?: {
@@ -19,5 +105,5 @@ export default function useCurrentFile() {
       speaker: string;
     }[];
   }>("current-file", {});
-  return { file, setFile };
+  return { file, setFile, loadFile };
 }
